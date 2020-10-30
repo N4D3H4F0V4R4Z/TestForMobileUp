6ff7ff8e0abc5fc4493c146fc7f56b893f49da67
Araz Nadzhafov <brainless11.5.1994@gmail.com>
2020-10-30 16:29:03 +0300

    MessagesFull.Storyboard binding

--- TestForMobileUp/MessagesEmpty/Cell/MessagesEmptyTableViewCell.swift
+++ TestForMobileUp/MessagesEmpty/Cell/MessagesEmptyTableViewCell.swift
@@ -10,15 +10,9 @@ import UIKit
 class MessagesEmptyTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nothingFoundLabelView: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
