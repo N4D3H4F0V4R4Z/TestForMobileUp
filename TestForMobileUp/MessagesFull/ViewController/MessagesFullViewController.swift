a34a8af3cac865fedf894513f1a3ee4e827fbfb2
Araz Nadzhafov <brainless11.5.1994@gmail.com>
2020-10-30 17:06:42 +0300

    URL + Error&Response Of dataTask

--- TestForMobileUp/MessagesFull/ViewController/MessagesFullViewController.swift
+++ TestForMobileUp/MessagesFull/ViewController/MessagesFullViewController.swift
@@ -24,6 +24,14 @@ class MessagesFullViewController: UIViewController {
     // Создаем Activity Indikator типа UIActivityIndicatorView
     var activityIndikator: UIActivityIndicatorView = UIActivityIndicatorView()
     
+    // - URL
+    
+    // Создаем глобальную сессию
+    private let session = URLSession.shared
+    
+    // Data
+    private let url = URL(string: "https://s3-eu-west-1.amazonaws.com/builds.getmobileup.com/storage/MobileUp-Test/api.json")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -86,6 +94,60 @@ extension MessagesFullViewController: UITableViewDelegate {
 }
 
 // MARK: -
+// MARK: - Server Request
+// Функция, отвечающая за получение данных из сети
+private extension MessagesFullViewController {
+    
+    // completionHandler - функция, которую мы "описали", но выполнится она только тогда, когда мы её вызовем, а вызовем мы её "изнутри" dataTask, то есть только тогда, когда к нам придет ответ с сервера; Closure - замыкание, анонимный блок кода, который выполняется в определенный момент времени; замыкаются данные, с которыми будут работать (data, response, error) и после in можно использовать, так как они замкнуты
+    func getMessages() {
+        let task = session.dataTask(with: url!, completionHandler: { [weak self] data, response, error in
+            
+            guard let strongSelf = self else {return}
+            
+            // Если закомментировать stopLoader(), то можно увидеть загружающийся индикатор
+            DispatchQueue.main.async {
+                strongSelf.stopLoader()
+            }
+            
+            if let error = error {
+                
+                let nothingFoundVC = UIStoryboard(name: "MessagesEmpty", bundle: nil).instantiateInitialViewController() as! MessagesEmptyViewController
+                self?.navigationController?.pushViewController(nothingFoundVC, animated: true)
+                
+                // localizedDescription помогает получить "адекватную" ошибку в виде текста; то есть, если ошибка будет, то мы её выведем и выйдем из функции, чтобы дальше не продолжать
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            // В response можно увидеть, какой у ответа статус-код, если ответ от 200 до 299, то ответ положительный, если ответ, например, 404, то ответ отрицательный
+            if let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) {
+                print("Ответ положительный: \(httpResponse.statusCode)")
+                
+                // Делаем декодирование и смотрим ответ с сервера
+                guard let data = data else {return}
+                if let json = try? JSONSerialization.jsonObject(with: data, options: []) {
+                    print(json)
+                }
+                
+                // Перезагружаем данные, так как изначально у нас в массиве persons - ноль обьектов, после они загружаются из JSON и обновляются; само обновление вкладвыается в closure DispatchQueue
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }
+            
+        })
+        
+        // Запрос серверу и выполнение функции
+        task.resume()
+        
+    }
+    
+    // Если не обращаться к [weak self], то будет утечка памяти, так как наша функция замыкания - reference type (ссылочный тип), то есть обьект "живет", пока на неё есть хотя бы одна ссылка, в нашем случае ссылка от MessagesViewController, но completionHandler внутри себя использует self сильной ссылкой, то есть получается, что completionHandler указывает на MessagesViewController и в итоге эти 2 ссылки не уходят из памяти; значит, нужно одну ссылку сделать weak, а к self привязать ?, чтобы связь стала тоже weak
+    
+}
+
+
+// MARK: -
 // MARK: - Configure
 
 private extension MessagesFullViewController {
@@ -93,6 +155,7 @@ private extension MessagesFullViewController {
     func configure() {
         configureTableView()
         startLoader()
+        getMessages()
     }
     
     func configureTableView() {
